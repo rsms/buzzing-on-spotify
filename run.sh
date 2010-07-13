6518adefa188bb5676027a39013d44006b2f19f0
Rasmus Andersson <rasmus@notion.se>
2010-07-13 23:55:50 +0200

    buffered feeding makes Spotify playlist system happier

--- run.sh
+++ run.sh
@@ -10,8 +10,19 @@ SPOTIFY_PASSWD_FILE=spotify-updater/password
 # Name of (existing) playlist in your Spotify account to be updated
 SPOTIFY_PLAYLIST_NAME='Buzzing'
 
+# Feed push2playlists through a buffer of {limit} links
+declare -a links
+index=0
+limit=5
 for f in $(python buzz-collector/main.py -k "$BUZZ_KEY" -s "$BUZZ_SECRET"); do
-  spotify-updater/push2playlist \
-    "$SPOTIFY_USER" "$(cat $SPOTIFY_PASSWD_FILE)" "$SPOTIFY_PLAYLIST_NAME" $f \
-    || exit $?
+  links[$index]=$f
+  ((index++))
+  if [ "$index" -eq "$limit" ]; then
+    spotify-updater/push2playlist \
+      "$SPOTIFY_USER" "$(cat $SPOTIFY_PASSWD_FILE)" \
+      "$SPOTIFY_PLAYLIST_NAME" ${links[@]} \
+      || exit $?
+    index=0
+    links=( )
+  fi
 done
